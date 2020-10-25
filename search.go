42342dd53555f6ae96ae7beadd52735635eefe31
hiracky16 <h.piiice16@gmail.com>
2020-10-25 14:30:34 +0900

    dynamono policy を追加

--- search.go
+++ search.go
@@ -3,13 +3,46 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/guregu/dynamo"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"fmt"
+	"os"
+	"encoding/json"
 )
 
+type AddressData struct {
+  Zipcode string `dynamo:"zipcode"`
+  Address string
+  IsOffce bool
+}
+
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	db := dynamo.New(session.New(), &aws.Config{
+    Region: aws.String(endpoints.ApNortheast1RegionID),
+  })
+  var tableName = os.Getenv("TABLE")
+	table := db.Table(tableName)
+
 	zipcode := request.QueryStringParameters["zipcode"]
-  fmt.Println("zipcode: ", zipcode)
-  return events.APIGatewayProxyResponse{Body: zipcode, StatusCode: 200}, nil
+	fmt.Println("zipcode: ", zipcode)
+
+	var result AddressData
+	err := table.Get("zipcode", zipcode).One(&result)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	res, err := json.Marshal(result)
+  if err != nil {
+    fmt.Println(err)
+		os.Exit(1)
+  }
+
+  return events.APIGatewayProxyResponse{Body: string(res), StatusCode: 200}, nil
 }
 
 func main() {
